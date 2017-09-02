28ac15d707e2a28d28c8daaf97d7dd55650ef21f
kalson <lijiaxiang1992@163.com>
2017-09-02 23:02:56 +0800

    Add orm module

--- DriverTest/day11-orm.py
+++ DriverTest/day11-orm.py
@@ -8,7 +8,7 @@ from  sqlalchemy.orm import  sessionmaker
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
-engine=create_engine("mysql+mysqldb://root:redhat123@192.168.242.129/awesome",encoding='utf-8',echo=True)
+engine=create_engine("mysql+mysqldb://root:redhat123@192.168.242.129/awesome",encoding='utf-8',echo=False)
 Base=declarative_base()
 class User(Base):
     __tablename__="user"
@@ -29,34 +29,34 @@ class Address(Base):
 Base.metadata.create_all(engine)
 Session_class=sessionmaker(bind=engine)
 Session=Session_class()
-user_obj=User(name='alex',password='alex3714')
-print (user_obj.name,user_obj.id)
-Session.add(user_obj)
-print (user_obj.name,user_obj.id)
-Session.commit()        #增加
-my_user=Session.query(User).filter_by(name='alex').first()
-print(my_user)
-print (my_user.id,my_user.name,my_user.password)#查询
-my_user.name="Alex Li"
-Session.commit()
-my_user = Session.query(User).filter_by(id=1).first()#回滚
-my_user.name = "Jack"
-fake_user=User(name='Rain',password='12345')
-Session.add(fake_user)
-print (Session.query(User).filter(User.name.in_(['Jack','Rain'])).all())
-Session.rollback() #此时你rollback一下
-print(Session.query(User).filter(User.name.in_(['Jack','rain'])).all() )
-print(Session.query(User.name,User.id).all() )
-Session.commit()
+# user_obj=User(name='alex',password='alex3714')
+# print (user_obj.name,user_obj.id)
+# Session.add(user_obj)
+# print (user_obj.name,user_obj.id)
+# Session.commit()        #增加
+# my_user=Session.query(User).filter_by(name='alex').first()
+# print(my_user)
+# print (my_user.id,my_user.name,my_user.password)#查询
+# my_user.name="Alex Li"
+# Session.commit()
+# my_user = Session.query(User).filter_by(id=1).first()#回滚
+# my_user.name = "Jack"
+# fake_user=User(name='Rain',password='12345')
+# Session.add(fake_user)
+# print (Session.query(User).filter(User.name.in_(['Jack','Rain'])).all())
+# Session.rollback() #此时你rollback一下
+# print(Session.query(User).filter(User.name.in_(['Jack','rain'])).all() )
+# print(Session.query(User.name,User.id).all() )
+# Session.commit()
 #========以上是对一个表的增删改查==========
 #========以下开始操作联动表格==============
 obj=Session.query(User).first()
 for i in obj.addresses:
     print(i)
-# addr_obj=Session.query(Address).first()
-# print (addr_obj.user.name)
+addr_obj=Session.query(Address).first()
+print (addr_obj.user.name)
 
-obj = Session.query(User).filter(User.name == 'rain').all()[0]
+obj = Session.query(User).filter(User.name== 'Rain').all()[0]
 print(obj.addresses)
 
 obj.addresses = [Address(email_address="r1@126.com"),  # 添加关联对象
